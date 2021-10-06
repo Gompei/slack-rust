f1fbf66a7d1b549ae33224e82a8263c0d79d57a4
Gompei <shoueigonpei6028@gmail.com>
2021-10-07 08:35:48 +0900

    add socket mode client

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+/// Implement this trait in your code to handle slack events
+pub trait SocketModeEventHandler {
+    fn on_hello() {}
+    fn on_events_api() {}
+    fn on_interactive() {}
+    fn on_disconnect() {}
+}
+
+/// The socket client
+pub struct SocketModeClient {}
+
+impl SocketModeClient {
+    pub async fn run<T: SocketModeEventHandler> () {
+        println!("Run");
     }
 }
