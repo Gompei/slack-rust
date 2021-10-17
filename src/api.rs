346b928d15b6d22597c4a5ee139a76bd1818c2d6
Gompei <shoueigonpei6028@gmail.com>
2021-10-17 19:47:39 +0900

    fix api.rs

--- src/api.rs
+++ src/api.rs
@@ -1,5 +1,10 @@
 const API_BASE_URL: &str = "https://slack.com/api/";
 
+/// The slack api client
+pub struct ApiClient {
+    pub token: Token,
+}
+
 pub struct Token {
     pub api_key: String,
     pub bot_key: String,
@@ -24,71 +29,21 @@ pub struct Query {
     pub view: String,
 }
 
-impl Token {
+impl ApiClient {
     pub async fn open_connection(&self) -> surf::Result<OpenConnectionsResponse> {
         surf::post(API_BASE_URL.to_string() + "apps.connections.open")
             .header(
                 surf::http::headers::AUTHORIZATION,
-                format!("Bearer {}", self.api_key),
+                format!("Bearer {}", self.token.api_key),
             )
             .recv_json()
             .await
     }
-
-    pub async fn open_view(&self, trigger: String) -> surf::Result<APIResponse> {
-        // TODO: テストモーダル
-        let view_modal = r#"{
-          "type": "modal",
-          "callback_id": "modal-with-inputs",
-          "title": {
-            "type": "plain_text",
-            "text": "Modal with inputs"
-          },
-          "blocks": [
-            {
-              "type": "input",
-              "block_id": "multiline",
-              "label": {
-                "type": "plain_text",
-                "text": "Enter your value"
-              },
-              "element": {
-                "type": "plain_text_input",
-                "multiline": true,
-                "action_id": "mlvalue"
-              }
-            },
-            {
-              "block_id": "target_channel",
-              "type": "input",
-              "optional": true,
-              "label": {
-                "type": "plain_text",
-                "text": "Select a channel to post the result on",
-              },
-              "element": {
-                "action_id": "target_select",
-                "type": "conversations_select",
-                "response_url_enabled": true,
-              },
-            }
-          ],
-          "submit": {
-            "type": "plain_text",
-            "text": "Submit"
-          }
-        }"#;
-
-        // クエリ作成
-        let query = Query {
-            trigger_id: trigger,
-            view: String::from(view_modal),
-        };
-
+    pub async fn open_view(&self, query: Query) -> surf::Result<APIResponse> {
         surf::post(API_BASE_URL.to_string() + "views.open")
             .header(
                 surf::http::headers::AUTHORIZATION,
-                format!("Bearer {}", self.bot_key),
+                format!("Bearer {}", self.token.bot_key),
             )
             .query(&query)?
             .recv_json()
