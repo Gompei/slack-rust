993483187a444027f11ed6ff139cdd2a08aa0e38
Gompei <shoueigonpei6028@gmail.com>
2021-12-11 11:57:54 +0900

    implement users list

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,5 @@ pub mod chat;
 pub mod error;
 pub mod http_client;
 pub mod socket_mode;
+pub mod users;
 pub mod views;
---
+++ src/users/list.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::http_client::{get_slack_url, post_json, Client, ResponseMetadata};
+use crate::users::user::User;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ListRequest {
+    pub cursor: Option<String>,
+    pub include_locale: Option<bool>,
+    pub limit: Option<i8>,
+    pub team_id: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ListResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub members: Option<Box<User>>,
+    pub cache_ts: Option<i8>,
+    pub response_metadata: Option<ResponseMetadata>,
+}
+
+pub async fn list(
+    client: Client,
+    param: ListRequest,
+    bot_token: String,
+) -> Result<ListResponse, Error> {
+    let url = get_slack_url("users.list");
+    let json = serde_json::to_string(&param)?;
+
+    post_json(client, url, json, bot_token)
+        .await?
+        .body_json()
+        .await
+        .map_err(|e| Error::SurfError(e))
+}
