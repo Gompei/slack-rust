b53f04f93e24bbfe2c68fff34dd8fe871b6f4f66
Gompei <shoueigonpei6028@gmail.com>
2021-12-13 13:49:38 +0900

    Implemented chat.delete api

---
+++ src/chat/delete.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::http_client::{get_slack_url, SlackWebAPIClient};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct DeleteRequest {
+    pub channel: String,
+    pub ts: String,
+    pub as_user: Option<bool>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct DeleteResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub ts: Option<String>,
+}
+
+pub async fn delete<T>(
+    client: &T,
+    param: &DeleteRequest,
+    bot_token: &str,
+) -> Result<DeleteResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("chat.delete");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<DeleteResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -1,2 +1,3 @@
+pub mod delete;
 pub mod message;
 pub mod post_message;
