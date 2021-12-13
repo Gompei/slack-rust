5b9f8022bfaf602797434bc5cc204aa5b5fb1647
Gompei <shoueigonpei6028@gmail.com>
2021-12-13 14:09:11 +0900

    Implemented chat.meMessage api

---
+++ src/chat/me_message.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::http_client::{get_slack_url, SlackWebAPIClient};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct MeMessageRequest {
+    pub channel: String,
+    pub text: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct MeMessageResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub ts: Option<String>,
+}
+
+pub async fn me_message<T>(
+    client: &T,
+    param: &MeMessageRequest,
+    bot_token: &str,
+) -> Result<MeMessageResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("chat.meMessage");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<MeMessageResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -1,5 +1,6 @@
 pub mod delete;
 pub mod delete_scheduled_message;
 pub mod get_permalink;
+pub mod me_message;
 pub mod message;
 pub mod post_message;
