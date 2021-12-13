6fcfd30c26d908544c556df52d11a171bb56eb3d
Gompei <shoueigonpei6028@gmail.com>
2021-12-13 14:05:05 +0900

    Implemented chat.getPermalink api

---
+++ src/chat/get_permalink.rs
@@ -0,0 +1,36 @@
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::http_client::{get_slack_url, SlackWebAPIClient};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct GetPermalinkRequest {
+    pub channel: String,
+    pub message_ts: String,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct GetPermalinkResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub permalink: Option<String>,
+}
+
+pub async fn get_permalink<T>(
+    client: &T,
+    param: &GetPermalinkRequest,
+    bot_token: &str,
+) -> Result<GetPermalinkResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("chat.getPermalink");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<GetPermalinkResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -1,4 +1,5 @@
 pub mod delete;
 pub mod delete_scheduled_message;
+pub mod get_permalink;
 pub mod message;
 pub mod post_message;
