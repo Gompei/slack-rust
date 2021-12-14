99e7115ab5f09736dee4cdc5cce477e603969fb1
Gompei <shoueigonpei6028@gmail.com>
2021-12-14 20:19:10 +0900

    Implemented auth.teams.list api

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,2 +1,3 @@
 pub mod revoke;
+pub mod teams_list;
 pub mod test;
---
+++ src/auth/teams_list.rs
@@ -0,0 +1,39 @@
+use serde::{Deserialize, Serialize};
+
+use crate::error::Error;
+use crate::http_client::{get_slack_url, ResponseMetadata, SlackWebAPIClient};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct TeamsListRequest {
+    pub cursor: Option<String>,
+    pub include_icon: Option<bool>,
+    pub limit: Option<i32>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct TeamsListResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    // TODO
+    // pub teams: Option<>
+    pub response_metadata: Option<ResponseMetadata>,
+}
+
+pub async fn teams_list<T>(
+    client: &T,
+    param: &TeamsListRequest,
+    bot_token: &str,
+) -> Result<TeamsListResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("auth.teams.list");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<TeamsListResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
