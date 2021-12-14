76ebbf2683e8bdfe9d9c7c168e70228d48aec726
Gompei <shoueigonpei6028@gmail.com>
2021-12-14 20:11:28 +0900

    Implemented chat.postEphemeral api

--- src/chat/mod.rs
+++ src/chat/mod.rs
@@ -3,6 +3,7 @@ pub mod delete_scheduled_message;
 pub mod get_permalink;
 pub mod me_message;
 pub mod message;
+pub mod post_ephemeral;
 pub mod post_message;
 pub mod schedule_message;
 pub mod scheduled_messages_list;
---
+++ src/chat/post_ephemeral.rs
@@ -0,0 +1,48 @@
+use serde::{Deserialize, Serialize};
+
+use crate::attachments::attachment::Attachment;
+use crate::block::blocks::Blocks;
+use crate::error::Error;
+use crate::http_client::{get_slack_url, SlackWebAPIClient};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PostEphemeralRequest {
+    pub channel: String,
+    pub text: String,
+    pub user: String,
+    pub as_user: Option<String>,
+    pub attachments: Option<Vec<Attachment>>,
+    pub blocks: Option<Vec<Blocks>>,
+    pub icon_emoji: Option<String>,
+    pub icon_url: Option<String>,
+    pub link_names: Option<bool>,
+    pub parse: Option<String>,
+    pub thread_ts: Option<String>,
+    pub username: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct PostEphemeralResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub message_ts: Option<String>,
+}
+
+pub async fn post_ephemeral<T>(
+    client: &T,
+    param: &PostEphemeralRequest,
+    bot_token: &str,
+) -> Result<PostEphemeralResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("chat.postEphemeral");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<PostEphemeralResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
