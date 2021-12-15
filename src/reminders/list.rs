f57b2145c9536da8656b614964d678e6aac6bfd8
Gompei <shoueigonpei6028@gmail.com>
2021-12-15 19:31:27 +0900

    Implemented reminders.list api

---
+++ src/reminders/list.rs
@@ -0,0 +1,35 @@
+use crate::error::Error;
+use crate::http_client::{get_slack_url, SlackWebAPIClient};
+use crate::reminders::add::Reminder;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ListRequest {
+    pub team_id: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct ListResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub reminders: Option<Vec<Reminder>>,
+}
+
+pub async fn list<T>(
+    client: &T,
+    param: &ListRequest,
+    bot_token: &str,
+) -> Result<ListResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("reminders.list");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<ListResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
--- src/reminders/mod.rs
+++ src/reminders/mod.rs
@@ -2,3 +2,4 @@ pub mod add;
 pub mod complete;
 pub mod delete;
 pub mod info;
+pub mod list;
