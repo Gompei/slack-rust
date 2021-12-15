a59fe6f0cc535fc672b893c599597a541ef7d4bc
Gompei <shoueigonpei6028@gmail.com>
2021-12-15 20:38:34 +0900

    Implemented team.billing.info api

---
+++ src/team/billing_info.rs
@@ -0,0 +1,21 @@
+use crate::error::Error;
+use crate::http_client::{get_slack_url, SlackWebAPIClient};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct BillingInfoResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub plan: Option<String>,
+}
+
+pub async fn billing_info<T>(client: &T, bot_token: &str) -> Result<BillingInfoResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("team.billing.info");
+
+    client.post(&url, bot_token).await.and_then(|result| {
+        serde_json::from_str::<BillingInfoResponse>(&result).map_err(Error::SerdeJsonError)
+    })
+}
--- src/team/mod.rs
+++ src/team/mod.rs
@@ -1,4 +1,5 @@
 pub mod access_logs;
 pub mod billable_info;
+pub mod billing_info;
 pub mod info;
 pub mod integration_logs;
