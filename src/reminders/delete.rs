96ec1ffd76671e2a6eaa8638c189f881efcaaacb
Gompei <shoueigonpei6028@gmail.com>
2021-12-15 18:09:08 +0900

    Implemented reminders.delete api

---
+++ src/reminders/delete.rs
@@ -0,0 +1,28 @@
+use crate::error::Error;
+use crate::http_client::{get_slack_url, DefaultResponse, SlackWebAPIClient};
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct DeleteRequest {
+    pub reminder: String,
+    pub team_id: Option<String>,
+}
+
+pub async fn delete<T>(
+    client: &T,
+    param: &DeleteRequest,
+    bot_token: &str,
+) -> Result<DefaultResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("reminders.delete");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<DefaultResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
--- src/reminders/mod.rs
+++ src/reminders/mod.rs
@@ -1,2 +1,3 @@
 pub mod add;
 pub mod complete;
+pub mod delete;
