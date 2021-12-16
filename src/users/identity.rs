81f6966427c683a52f5423f741601e7059b6d801
Gompei <shoueigonpei6028@gmail.com>
2021-12-16 14:05:03 +0900

    Implemented users.identity api

---
+++ src/users/identity.rs
@@ -0,0 +1,24 @@
+use crate::error::Error;
+use crate::http_client::{get_slack_url, ResponseMetadata, SlackWebAPIClient};
+use crate::team::info::Team;
+use crate::users::user::User;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct IdentityResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub user: Option<User>,
+    pub team: Option<Team>,
+}
+
+pub async fn identity<T>(client: &T, bot_token: &str) -> Result<IdentityResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("users.identity");
+
+    client.post(&url, bot_token).await.and_then(|result| {
+        serde_json::from_str::<IdentityResponse>(&result).map_err(Error::SerdeJsonError)
+    })
+}
--- src/users/mod.rs
+++ src/users/mod.rs
@@ -1,5 +1,6 @@
 pub mod conversations;
 pub mod delete_photo;
 pub mod get_presence;
+pub mod identity;
 pub mod list;
 pub mod user;
