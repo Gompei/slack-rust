c38ba03981f749289ff2dcade217790998598153
Gompei <shoueigonpei6028@gmail.com>
2021-12-16 15:39:53 +0900

    Implemented usergroups.update api

--- src/usergroups/mod.rs
+++ src/usergroups/mod.rs
@@ -2,4 +2,5 @@ pub mod create;
 pub mod disable;
 pub mod enable;
 pub mod list;
+pub mod update;
 pub mod usergroup;
---
+++ src/usergroups/update.rs
@@ -0,0 +1,41 @@
+use crate::error::Error;
+use crate::http_client::{get_slack_url, SlackWebAPIClient};
+use crate::usergroups::usergroup::Usergroup;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct UpdateRequest {
+    pub usergroup: String,
+    pub channels: Option<String>,
+    pub description: Option<String>,
+    pub handle: Option<String>,
+    pub include_count: Option<bool>,
+    pub name: Option<String>,
+    pub team_id: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct UpdateResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub usergroups: Option<Vec<Usergroup>>,
+}
+
+pub async fn update<T>(
+    client: &T,
+    param: &UpdateRequest,
+    bot_token: &str,
+) -> Result<UpdateResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("usergroups.update");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<UpdateResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
