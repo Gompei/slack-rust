473833416a04dbbf9981f6964cd5306111f50569
Gompei <shoueigonpei6028@gmail.com>
2021-12-16 15:44:40 +0900

    Implemented usergroups.users.update api

---
+++ src/usergroups/users/update.rs
@@ -0,0 +1,38 @@
+use crate::error::Error;
+use crate::http_client::{get_slack_url, SlackWebAPIClient};
+use crate::usergroups::usergroup::Usergroup;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct UpdateRequest {
+    pub usergroup: String,
+    pub users: String,
+    pub include_count: Option<bool>,
+    pub team_id: Option<String>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct UpdateResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub usergroup: Option<Usergroup>,
+}
+
+pub async fn update<T>(
+    client: &T,
+    param: &UpdateRequest,
+    bot_token: &str,
+) -> Result<UpdateResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("usergroups.users.update");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<UpdateResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
