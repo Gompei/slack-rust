f14c32c4f3a3c29e44ae0c5facf9d5338b5380e1
Gompei <shoueigonpei6028@gmail.com>
2021-12-16 14:08:34 +0900

    Implemented users.info api

---
+++ src/users/info.rs
@@ -0,0 +1,36 @@
+use crate::error::Error;
+use crate::http_client::{get_slack_url, ResponseMetadata, SlackWebAPIClient};
+use crate::users::user::User;
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct InfoRequest {
+    pub user: String,
+    pub include_locale: Option<bool>,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct InfoResponse {
+    pub ok: bool,
+    pub error: Option<String>,
+    pub user: Option<User>,
+}
+
+pub async fn info<T>(
+    client: &T,
+    param: &InfoRequest,
+    bot_token: &str,
+) -> Result<InfoResponse, Error>
+where
+    T: SlackWebAPIClient,
+{
+    let url = get_slack_url("users.info");
+    let json = serde_json::to_string(&param)?;
+
+    client
+        .post_json(&url, &json, bot_token)
+        .await
+        .and_then(|result| {
+            serde_json::from_str::<InfoResponse>(&result).map_err(Error::SerdeJsonError)
+        })
+}
--- src/users/mod.rs
+++ src/users/mod.rs
@@ -2,5 +2,6 @@ pub mod conversations;
 pub mod delete_photo;
 pub mod get_presence;
 pub mod identity;
+pub mod info;
 pub mod list;
 pub mod user;
