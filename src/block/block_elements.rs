a077302135bf6bca013df682847aca2ffc00fbd7
Gompei <shoueigonpei6028@gmail.com>
2021-12-18 17:51:07 +0900

    Implemented BlockElement

--- src/block/block_elements.rs
+++ src/block/block_elements.rs
@@ -1,5 +1,6 @@
 use crate::block::block_object::{
-    ConfirmationBlockObject, OptionBlockObject, OptionGroupBlockObject, TextBlockObject,
+    ConfirmationBlockObject, DispatchActionConfig, OptionBlockObject, OptionGroupBlockObject,
+    TextBlockObject,
 };
 use serde::{Deserialize, Serialize};
 use std::fmt;
@@ -75,6 +76,131 @@ pub struct ButtonElement {
 
 #[typetag::serde]
 impl BlockElement for ButtonElement {
+    fn element_type(&self) -> &String {
+        &self.r#type
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct CheckboxGroupsBlockElement {
+    pub r#type: String,
+    pub action_id: String,
+    pub options: Vec<OptionBlockObject>,
+    pub initial_options: Option<Vec<OptionBlockObject>>,
+    pub confirm: Option<ConfirmationBlockObject>,
+    pub focus_on_load: Option<bool>,
+}
+
+#[typetag::serde]
+impl BlockElement for CheckboxGroupsBlockElement {
+    fn element_type(&self) -> &String {
+        &self.r#type
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct DatePickerBlockElement {
+    pub r#type: String,
+    pub action_id: String,
+    pub placeholder: Option<TextBlockObject>,
+    pub initial_date: Option<String>,
+    pub confirm: Option<ConfirmationBlockObject>,
+    pub focus_on_load: Option<bool>,
+}
+
+#[typetag::serde]
+impl BlockElement for DatePickerBlockElement {
+    fn element_type(&self) -> &String {
+        &self.r#type
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct MultiSelectBlockElement {
+    pub r#type: String,
+    pub placeholder: TextBlockObject,
+    pub action_id: String,
+    pub options: Vec<OptionBlockObject>,
+    pub option_groups: Option<Vec<OptionGroupBlockObject>>,
+    pub initial_option: Option<OptionBlockObject>,
+    pub initial_users: Option<Vec<String>>,
+    pub initial_conversations: Option<Vec<String>>,
+    pub initial_channels: Option<Vec<String>>,
+    pub confirm: Option<ConfirmationBlockObject>,
+    pub min_query_length: Option<i32>,
+    pub max_selected_items: Option<i32>,
+    pub focus_on_load: Option<bool>,
+}
+
+#[typetag::serde]
+impl BlockElement for MultiSelectBlockElemen {
+    fn element_type(&self) -> &String {
+        &self.r#type
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct OverflowBlockElement {
+    pub r#type: String,
+    pub action_id: String,
+    pub options: Vec<OptionBlockObject>,
+    pub confirm: Option<ConfirmationBlockObject>,
+}
+
+#[typetag::serde]
+impl BlockElement for OverflowBlockElement {
+    fn element_type(&self) -> &String {
+        &self.r#type
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct PlainTextInputBlockElement {
+    pub r#type: String,
+    pub action_id: String,
+    pub placeholder: Option<TextBlockObject>,
+    pub initial_value: Option<String>,
+    pub multiline: Option<bool>,
+    pub min_length: Option<i32>,
+    pub max_length: Option<i32>,
+    pub dispatch_action_config: Option<DispatchActionConfig>,
+    pub focus_on_load: Option<bool>,
+}
+
+#[typetag::serde]
+impl BlockElement for PlainTextInputBlockElement {
+    fn element_type(&self) -> &String {
+        &self.r#type
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct RadioButtonsBlockElement {
+    pub r#type: String,
+    pub action_id: String,
+    pub options: Vec<OptionBlockObject>,
+    pub initial_option: Option<OptionBlockObject>,
+    pub confirm: Option<ConfirmationBlockObject>,
+}
+
+#[typetag::serde]
+impl BlockElement for RadioButtonsBlockElement {
+    fn element_type(&self) -> &String {
+        &self.r#type
+    }
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct TimePickerBlockElement {
+    pub r#type: String,
+    pub action_id: String,
+    pub placeholder: Option<TextBlockObject>,
+    pub initial_time: Option<String>,
+    pub confirm: Option<ConfirmationBlockObject>,
+}
+
+#[typetag::serde]
+impl BlockElement for TimePickerBlockElement {
     fn element_type(&self) -> &String {
         &self.r#type
     }
