bff61b6103e751d288426d8e3072523593dc4c55
Gompei <shoueigonpei6028@gmail.com>
2021-12-25 00:22:42 +0900

    add SlackToken struct

--- src/socket/socket_mode.rs
+++ src/socket/socket_mode.rs
@@ -19,27 +19,40 @@ pub type Stream = WebSocketStream<TlsStream<TcpStream>>;
 /// Implement this trait in your code to handle slack events.
 #[async_trait]
 pub trait EventHandler {
-    async fn on_close(&mut self, _client: &Client) {
+    async fn on_close(&mut self, _client: &Client, _token: &SlackToken) {
         log::info!("websocket close");
     }
-    async fn on_connect(&mut self, _client: &Client) {
+    async fn on_connect(&mut self, _client: &Client, _token: &SlackToken) {
         log::info!("websocket connect");
     }
-    async fn on_hello(&mut self, _client: &Client, e: &HelloEvent) {
+    async fn on_hello(&mut self, _client: &Client, _token: &SlackToken, e: &HelloEvent) {
         log::info!("hello event: {:?}", e);
     }
-    async fn on_disconnect(&mut self, _client: &Client, e: &DisconnectEvent) {
+    async fn on_disconnect(&mut self, _client: &Client, _token: &SlackToken, e: &DisconnectEvent) {
         log::info!("disconnect event: {:?}", e);
     }
-    async fn on_events_api(&mut self, _client: &Client, e: &EventsAPI, _s: &mut Stream) {
+    async fn on_events_api(
+        &mut self,
+        _client: &Client,
+        _token: &SlackToken,
+        e: &EventsAPI,
+        _s: &mut Stream,
+    ) {
         log::info!("events api event: {:?}", e);
     }
-    async fn on_interactive(&mut self, _client: &Client, e: &InteractiveEvent, _s: &mut Stream) {
+    async fn on_interactive(
+        &mut self,
+        _client: &Client,
+        _token: &SlackToken,
+        e: &InteractiveEvent,
+        _s: &mut Stream,
+    ) {
         log::info!("interactive event: {:?}", e);
     }
     async fn on_slash_commands(
         &mut self,
         _client: &Client,
+        _token: &SlackToken,
         e: &SlashCommandsEvent,
         _s: &mut Stream,
     ) {
@@ -52,12 +65,12 @@ pub struct SocketMode {}
 
 impl SocketMode {
     /// Run slack and websocket communication.
-    pub async fn run<T>(app_token: &str, handler: &mut T) -> Result<(), Error>
+    pub async fn run<T>(token: &SlackToken, handler: &mut T) -> Result<(), Error>
     where
         T: EventHandler + std::marker::Send,
     {
         let client = default_client();
-        let wss_url = connections_open(&client, app_token).await?;
+        let wss_url = connections_open(&client, &token.slack_app_token).await?;
         let url = wss_url
             .url
             .ok_or_else(|| Error::OptionError("connections open error".to_string()))?;
@@ -73,7 +86,7 @@ impl SocketMode {
 
         let (mut stream, _) = client_async(url, tls_stream).await?;
 
-        handler.on_connect(&client).await;
+        handler.on_connect(&client, token).await;
 
         loop {
             let message = stream
@@ -85,18 +98,24 @@ impl SocketMode {
                 Message::Text(t) => {
                     let event = serde_json::from_str::<SocketModeEvent>(&t)?;
                     match event {
-                        SocketModeEvent::HelloEvent(e) => handler.on_hello(&client, &e).await,
+                        SocketModeEvent::HelloEvent(e) => {
+                            handler.on_hello(&client, token, &e).await
+                        }
                         SocketModeEvent::DisconnectEvent(e) => {
-                            handler.on_disconnect(&client, &e).await
+                            handler.on_disconnect(&client, token, &e).await
                         }
                         SocketModeEvent::EventsAPI(e) => {
-                            handler.on_events_api(&client, &e, &mut stream).await
+                            handler.on_events_api(&client, token, &e, &mut stream).await
                         }
                         SocketModeEvent::InteractiveEvent(e) => {
-                            handler.on_interactive(&client, &e, &mut stream).await
+                            handler
+                                .on_interactive(&client, token, &e, &mut stream)
+                                .await
                         }
                         SocketModeEvent::SlashCommandsEvent(e) => {
-                            handler.on_slash_commands(&client, &e, &mut stream).await
+                            handler
+                                .on_slash_commands(&client, token, &e, &mut stream)
+                                .await
                         }
                     }
                 }
@@ -114,4 +133,9 @@ impl SocketMode {
             .await
             .map_err(Error::WebSocketError)
     }
+}
+
+pub struct SlackToken {
+    pub slack_app_token: String,
+    pub slack_bot_token: String,
 }
