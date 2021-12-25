bb8a8fbffa0ec04dc539b843b62957b572e60fa8
Gompei <shoueigonpei6028@gmail.com>
2021-12-25 20:00:09 +0900

    add chat.delete test code

--- src/chat/delete.rs
+++ src/chat/delete.rs
@@ -1,7 +1,9 @@
 use crate::error::Error;
-use crate::http_client::{get_slack_url, SlackWebAPIClient};
+use crate::http_client::{get_slack_url, ResponseMetadata, SlackWebAPIClient};
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct DeleteRequest {
     pub channel: String,
@@ -9,10 +11,13 @@ pub struct DeleteRequest {
     pub as_user: Option<bool>,
 }
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct DeleteResponse {
     pub ok: bool,
     pub error: Option<String>,
+    pub response_metadata: Option<ResponseMetadata>,
+    pub channel: Option<String>,
     pub ts: Option<String>,
 }
 
@@ -33,4 +38,82 @@ where
         .and_then(|result| {
             serde_json::from_str::<DeleteResponse>(&result).map_err(Error::SerdeJsonError)
         })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::http_client::MockSlackWebAPIClient;
+
+    #[test]
+    fn convert_request() {
+        let request = DeleteRequest {
+            channel: "C1234567890".to_string(),
+            ts: "1405894322.002768".to_string(),
+            as_user: Some(true),
+        };
+        let json = r##"{
+  "channel": "C1234567890",
+  "ts": "1405894322.002768",
+  "as_user": true
+}"##;
+
+        let j = serde_json::to_string_pretty(&request).unwrap();
+        assert_eq!(json, j);
+
+        let s = serde_json::from_str::<DeleteRequest>(json).unwrap();
+        assert_eq!(request, s);
+    }
+
+    #[test]
+    fn convert_response() {
+        let response = DeleteResponse {
+            ok: true,
+            channel: Some("C1234567890".to_string()),
+            ts: Some("1405894322.002768".to_string()),
+            ..Default::default()
+        };
+        let json = r##"{
+  "ok": true,
+  "channel": "C1234567890",
+  "ts": "1405894322.002768"
+}"##;
+
+        let j = serde_json::to_string_pretty(&response).unwrap();
+        assert_eq!(json, j);
+
+        let s = serde_json::from_str::<DeleteResponse>(json).unwrap();
+        assert_eq!(response, s);
+    }
+
+    #[async_std::test]
+    async fn test_delete() {
+        let param = DeleteRequest {
+            channel: "C1234567890".to_string(),
+            ts: "1405894322.002768".to_string(),
+            as_user: Some(true),
+        };
+
+        let mut mock = MockSlackWebAPIClient::new();
+        mock.expect_post_json().returning(|_, _, _| {
+            Ok(r##"{
+  "ok": true,
+  "channel": "C1234567890",
+  "ts": "1405894322.002768"
+}"##
+            .to_string())
+        });
+
+        let response = delete(&mock, &param, &"test_token".to_string())
+            .await
+            .unwrap();
+        let expect = DeleteResponse {
+            ok: true,
+            channel: Some("C1234567890".to_string()),
+            ts: Some("1405894322.002768".to_string()),
+            ..Default::default()
+        };
+
+        assert_eq!(expect, response);
+    }
 }
