d8d6480798df215462418429d4943b2e805eb5eb
Gompei <shoueigonpei6028@gmail.com>
2021-12-25 19:38:27 +0900

    add auth.test test code

--- src/auth/test.rs
+++ src/auth/test.rs
@@ -1,8 +1,9 @@
-use serde::{Deserialize, Serialize};
-
 use crate::error::Error;
 use crate::http_client::{get_slack_url, ResponseMetadata, SlackWebAPIClient};
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct TestResponse {
     pub ok: bool,
@@ -24,4 +25,66 @@ where
     client.post(&url, bot_token).await.and_then(|result| {
         serde_json::from_str::<TestResponse>(&result).map_err(Error::SerdeJsonError)
     })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::http_client::MockSlackWebAPIClient;
+
+    #[test]
+    fn convert_response() {
+        let response = TestResponse {
+            ok: true,
+            url: Some("https://subarachnoid.slack.com/".to_string()),
+            team: Some("Subarachnoid Workspace".to_string()),
+            user: Some("grace".to_string()),
+            team_id: Some("T12345678".to_string()),
+            user_id: Some("W12345678".to_string()),
+            ..Default::default()
+        };
+        let json = r##"{
+  "ok": true,
+  "url": "https://subarachnoid.slack.com/",
+  "team": "Subarachnoid Workspace",
+  "user": "grace",
+  "team_id": "T12345678",
+  "user_id": "W12345678"
+}"##;
+
+        let j = serde_json::to_string_pretty(&response).unwrap();
+        assert_eq!(json, j);
+
+        let s = serde_json::from_str::<TestResponse>(json).unwrap();
+        assert_eq!(response, s);
+    }
+
+    #[async_std::test]
+    async fn test_auth_test() {
+        let mut mock = MockSlackWebAPIClient::new();
+        mock.expect_post().returning(|_, _| {
+            Ok(r##"{
+          "ok": true,
+          "url": "https://subarachnoid.slack.com/",
+          "team": "Subarachnoid Workspace",
+          "user": "grace",
+          "team_id": "T12345678",
+          "user_id": "W12345678"  
+        }"##
+            .to_string())
+        });
+
+        let response = test(&mock, &"test_token".to_string()).await.unwrap();
+        let expect = TestResponse {
+            ok: true,
+            url: Some("https://subarachnoid.slack.com/".to_string()),
+            team: Some("Subarachnoid Workspace".to_string()),
+            user: Some("grace".to_string()),
+            team_id: Some("T12345678".to_string()),
+            user_id: Some("W12345678".to_string()),
+            ..Default::default()
+        };
+
+        assert_eq!(expect, response);
+    }
 }
