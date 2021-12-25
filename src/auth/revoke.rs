4728080d0f31fa21125d0e3428969997644b4734
Gompei <shoueigonpei6028@gmail.com>
2021-12-25 17:46:25 +0900

    add auth.revoke test code

--- src/auth/revoke.rs
+++ src/auth/revoke.rs
@@ -1,13 +1,15 @@
-use serde::{Deserialize, Serialize};
-
 use crate::error::Error;
 use crate::http_client::{get_slack_url, SlackWebAPIClient};
+use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct RevokeRequest {
     pub test: Option<bool>,
 }
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct RevokeResponse {
     pub ok: bool,
@@ -32,4 +34,68 @@ where
         .and_then(|result| {
             serde_json::from_str::<RevokeResponse>(&result).map_err(Error::SerdeJsonError)
         })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::http_client::MockSlackWebAPIClient;
+
+    #[test]
+    fn convert_request() {
+        let request = RevokeRequest { test: Some(true) };
+        let json = r##"{
+  "test": true
+}"##;
+
+        let j = serde_json::to_string_pretty(&request).unwrap();
+        assert_eq!(json, j);
+
+        let s = serde_json::from_str::<RevokeRequest>(json).unwrap();
+        assert_eq!(request, s);
+    }
+
+    #[test]
+    fn convert_response() {
+        let response = RevokeResponse {
+            ok: true,
+            revoked: Some(true),
+            ..Default::default()
+        };
+        let json = r##"{
+  "ok": true,
+  "revoked": true
+}"##;
+
+        let j = serde_json::to_string_pretty(&response).unwrap();
+        assert_eq!(json, j);
+
+        let s = serde_json::from_str::<RevokeResponse>(json).unwrap();
+        assert_eq!(response, s);
+    }
+
+    #[async_std::test]
+    async fn test_revoke() {
+        let param = RevokeRequest { test: Some(false) };
+
+        let mut mock = MockSlackWebAPIClient::new();
+        mock.expect_post_json().returning(|_, _, _| {
+            Ok(r##"{
+          "ok": true,
+          "revoked": true
+        }"##
+            .to_string())
+        });
+
+        let response = revoke(&mock, &param, &"test_token".to_string())
+            .await
+            .unwrap();
+        let expect = RevokeResponse {
+            ok: true,
+            revoked: Some(true),
+            ..Default::default()
+        };
+
+        assert_eq!(expect, response);
+    }
 }
