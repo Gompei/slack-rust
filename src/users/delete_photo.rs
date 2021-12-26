ad23c3343148c53f230f33c73d0d8f26c0e4b371
Gompei <shoueigonpei6028@gmail.com>
2021-12-26 22:08:42 +0900

    add users.deletePhoto test code

--- src/users/delete_photo.rs
+++ src/users/delete_photo.rs
@@ -11,3 +11,30 @@ where
         serde_json::from_str::<DefaultResponse>(&result).map_err(Error::SerdeJsonError)
     })
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::http_client::MockSlackWebAPIClient;
+
+    #[async_std::test]
+    async fn test_delete_photo() {
+        let mut mock = MockSlackWebAPIClient::new();
+        mock.expect_post().returning(|_, _| {
+            Ok(r##"{
+  "ok": true
+}"##
+            .to_string())
+        });
+
+        let response = delete_photo(&mock, &"test_token".to_string())
+            .await
+            .unwrap();
+        let expect = DefaultResponse {
+            ok: true,
+            ..Default::default()
+        };
+
+        assert_eq!(expect, response);
+    }
+}
