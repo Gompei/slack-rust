8415ad1f7ce4aaf50dd2bc6eacc7e855e15c96e6
Gompei <shoueigonpei6028@gmail.com>
2021-12-26 15:28:37 +0900

    fix reaction.rs

--- src/reactions/reaction.rs
+++ src/reactions/reaction.rs
@@ -1,5 +1,7 @@
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct Reaction {
     pub count: Option<i32>,
