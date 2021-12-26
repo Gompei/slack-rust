f169a3b1e21e34c4aeac45513a75c8b03313126f
Gompei <shoueigonpei6028@gmail.com>
2021-12-26 14:47:25 +0900

    fix Item struct

--- src/items/item.rs
+++ src/items/item.rs
@@ -2,10 +2,13 @@ use crate::chat::message::Message;
 use crate::comments::comment::Comment;
 use crate::files::file::File;
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct Item {
-    pub r#type: Option<String>,
+    #[serde(rename = "type")]
+    pub type_filed: Option<String>,
     pub channel: Option<String>,
     pub message: Option<Message>,
     pub file: Option<File>,
