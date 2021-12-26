2d800f40d227d1ae7ab558711bd074bed9eb2506
Gompei <shoueigonpei6028@gmail.com>
2021-12-26 15:25:44 +0900

    add reactions.list test code

--- src/chat/message.rs
+++ src/chat/message.rs
@@ -1,5 +1,6 @@
 use crate::attachment::attachment::Attachment;
 use crate::block::blocks::Block;
+use crate::reactions::reaction::Reaction;
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 
@@ -11,9 +12,11 @@ pub struct Message {
     pub type_file: Option<String>,
     pub text: Option<String>,
     pub user: Option<String>,
+    pub username: Option<String>,
     pub ts: Option<String>,
     pub team: Option<String>,
     pub attachments: Option<Vec<Attachment>>,
     pub blocks: Option<Vec<Block>>,
     pub subtype: Option<String>,
+    pub reactions: Option<Vec<Reaction>>,
 }
--- src/reactions/list.rs
+++ src/reactions/list.rs
@@ -1,9 +1,10 @@
 use crate::error::Error;
-
 use crate::http_client::{get_slack_url, ResponseMetadata, SlackWebAPIClient};
 use crate::items::item::Item;
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct ListRequest {
     pub count: Option<i32>,
@@ -15,12 +16,15 @@ pub struct ListRequest {
     pub user: Option<String>,
 }
 
+#[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct ListResponse {
     pub ok: bool,
     pub error: Option<String>,
-    pub items: Option<Item>,
     pub response_metadata: Option<ResponseMetadata>,
+    // TODO
+    // https://api.slack.com/methods/reactions.list#examples
+    pub items: Option<Vec<Item>>,
 }
 
 pub async fn list<T>(
@@ -40,4 +44,168 @@ where
         .and_then(|result| {
             serde_json::from_str::<ListResponse>(&result).map_err(Error::SerdeJsonError)
         })
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::chat::message::Message;
+    use crate::http_client::MockSlackWebAPIClient;
+    use crate::reactions::reaction::Reaction;
+
+    #[test]
+    fn convert_request() {
+        let request = ListRequest {
+            count: Some(20),
+            full: Some(true),
+            limit: Some(20),
+            page: Some(2),
+            team_id: Some("T1234567890".to_string()),
+            cursor: Some("dXNlcjpVMDYxTkZUVDI=".to_string()),
+            user: Some("W1234567890".to_string()),
+        };
+        let json = r##"{
+  "count": 20,
+  "cursor": "dXNlcjpVMDYxTkZUVDI=",
+  "full": true,
+  "limit": 20,
+  "page": 2,
+  "team_id": "T1234567890",
+  "user": "W1234567890"
+}"##;
+
+        let j = serde_json::to_string_pretty(&request).unwrap();
+        assert_eq!(json, j);
+
+        let s = serde_json::from_str::<ListRequest>(json).unwrap();
+        assert_eq!(request, s);
+    }
+
+    #[test]
+    fn convert_response() {
+        let response = ListResponse {
+            ok: true,
+            items: Some(vec![Item {
+                type_filed: Some("message".to_string()),
+                channel: Some("C3UKJTQAC".to_string()),
+                message: Some(Message {
+                    bot_id: Some("B4VLRLMKJ".to_string()),
+                    text: Some("Hello from Python! :tada:".to_string()),
+                    username: Some("Shipit Notifications".to_string()),
+                    ts: Some("1507849573.000090".to_string()),
+                    subtype: Some("bot_message".to_string()),
+                    reactions: Some(vec![Reaction {
+                        count: Some(1),
+                        name: Some("robot_face".to_string()),
+                        users: Some(vec!["U2U85N1RV".to_string()]),
+                    }]),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }]),
+            ..Default::default()
+        };
+        let json = r##"{
+  "ok": true,
+  "items": [
+    {
+      "type": "message",
+      "channel": "C3UKJTQAC",
+      "message": {
+        "bot_id": "B4VLRLMKJ",
+        "text": "Hello from Python! :tada:",
+        "username": "Shipit Notifications",
+        "ts": "1507849573.000090",
+        "subtype": "bot_message",
+        "reactions": [
+          {
+            "count": 1,
+            "name": "robot_face",
+            "users": [
+              "U2U85N1RV"
+            ]
+          }
+        ]
+      }
+    }
+  ]
+}"##;
+
+        let j = serde_json::to_string_pretty(&response).unwrap();
+        assert_eq!(json, j);
+
+        let s = serde_json::from_str::<ListResponse>(json).unwrap();
+        assert_eq!(response, s);
+    }
+
+    #[async_std::test]
+    async fn test_list() {
+        let param = ListRequest {
+            count: Some(20),
+            full: Some(true),
+            limit: Some(20),
+            page: Some(2),
+            team_id: Some("T1234567890".to_string()),
+            cursor: Some("dXNlcjpVMDYxTkZUVDI=".to_string()),
+            user: Some("W1234567890".to_string()),
+        };
+
+        let mut mock = MockSlackWebAPIClient::new();
+        mock.expect_post_json().returning(|_, _, _| {
+            Ok(r##"{
+  "ok": true,
+  "items": [
+    {
+      "type": "message",
+      "channel": "C3UKJTQAC",
+      "message": {
+        "bot_id": "B4VLRLMKJ",
+        "text": "Hello from Python! :tada:",
+        "username": "Shipit Notifications",
+        "ts": "1507849573.000090",
+        "subtype": "bot_message",
+        "reactions": [
+          {
+            "count": 1,
+            "name": "robot_face",
+            "users": [
+              "U2U85N1RV"
+            ]
+          }
+        ]
+      }
+    }
+  ]
+}"##
+            .to_string())
+        });
+
+        let response = list(&mock, &param, &"test_token".to_string())
+            .await
+            .unwrap();
+        let expect = ListResponse {
+            ok: true,
+            items: Some(vec![Item {
+                type_filed: Some("message".to_string()),
+                channel: Some("C3UKJTQAC".to_string()),
+                message: Some(Message {
+                    bot_id: Some("B4VLRLMKJ".to_string()),
+                    text: Some("Hello from Python! :tada:".to_string()),
+                    username: Some("Shipit Notifications".to_string()),
+                    ts: Some("1507849573.000090".to_string()),
+                    subtype: Some("bot_message".to_string()),
+                    reactions: Some(vec![Reaction {
+                        count: Some(1),
+                        name: Some("robot_face".to_string()),
+                        users: Some(vec!["U2U85N1RV".to_string()]),
+                    }]),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            }]),
+            ..Default::default()
+        };
+
+        assert_eq!(expect, response);
+    }
 }
