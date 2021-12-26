93c5709145695b1eedbba93f72f95b251b5a9b15
Gompei <shoueigonpei6028@gmail.com>
2021-12-26 14:51:53 +0900

    add reactions.add test code

--- src/reactions/add.rs
+++ src/reactions/add.rs
@@ -27,3 +27,54 @@ where
             serde_json::from_str::<DefaultResponse>(&result).map_err(Error::SerdeJsonError)
         })
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::http_client::MockSlackWebAPIClient;
+
+    #[test]
+    fn convert_request() {
+        let request = AddRequest {
+            channel: "C1234567890".to_string(),
+            name: "thumbsup".to_string(),
+            timestamp: "1234567890.123456".to_string(),
+        };
+        let json = r##"{
+  "channel": "C1234567890",
+  "name": "thumbsup",
+  "timestamp": "1234567890.123456"
+}"##;
+
+        let j = serde_json::to_string_pretty(&request).unwrap();
+        assert_eq!(json, j);
+
+        let s = serde_json::from_str::<AddRequest>(json).unwrap();
+        assert_eq!(request, s);
+    }
+
+    #[async_std::test]
+    async fn test_add() {
+        let param = AddRequest {
+            channel: "C1234567890".to_string(),
+            name: "thumbsup".to_string(),
+            timestamp: "1234567890.123456".to_string(),
+        };
+
+        let mut mock = MockSlackWebAPIClient::new();
+        mock.expect_post_json().returning(|_, _, _| {
+            Ok(r##"{
+  "ok": true
+}"##
+            .to_string())
+        });
+
+        let response = add(&mock, &param, &"test_token".to_string()).await.unwrap();
+        let expect = DefaultResponse {
+            ok: true,
+            ..Default::default()
+        };
+
+        assert_eq!(expect, response);
+    }
+}
