2c8c7d15d6fff2608334850b834850f6785fb638
Gompei <shoueigonpei6028@gmail.com>
2021-12-31 11:39:52 +0900

    add SectionBlockBuilder

--- src/block/block_section.rs
+++ src/block/block_section.rs
@@ -6,9 +6,54 @@ use serde_with::skip_serializing_none;
 #[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct SectionBlock {
+    pub text: Option<TextBlockObject>,
     pub block_id: Option<String>,
-    pub text: TextBlockObject,
-    pub element: Option<BlockElement>,
     pub fields: Option<Vec<TextBlockObject>>,
     pub accessory: Option<BlockElement>,
+}
+
+impl SectionBlock {
+    pub fn builder() -> SectionBlockBuilder {
+        SectionBlockBuilder::new()
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct SectionBlockBuilder {
+    pub text: Option<TextBlockObject>,
+    pub block_id: Option<String>,
+    pub fields: Option<Vec<TextBlockObject>>,
+    pub accessory: Option<BlockElement>,
+}
+
+impl SectionBlockBuilder {
+    pub fn new() -> SectionBlockBuilder {
+        SectionBlockBuilder {
+            ..Default::default()
+        }
+    }
+    pub fn text(mut self, text: TextBlockObject) -> SectionBlockBuilder {
+        self.text = Some(text);
+        self
+    }
+    pub fn block_id(mut self, block_id: String) -> SectionBlockBuilder {
+        self.block_id = Some(block_id);
+        self
+    }
+    pub fn fields(mut self, fields: Vec<TextBlockObject>) -> SectionBlockBuilder {
+        self.fields = Some(fields);
+        self
+    }
+    pub fn accessory(mut self, accessory: BlockElement) -> SectionBlockBuilder {
+        self.accessory = Some(accessory);
+        self
+    }
+    pub fn build(self) -> SectionBlock {
+        SectionBlock {
+            text: self.text,
+            block_id: self.block_id,
+            fields: self.fields,
+            accessory: self.accessory,
+        }
+    }
 }
