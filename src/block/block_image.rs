f97895c01ded014c433499b43a58ece966b0076f
Gompei <shoueigonpei6028@gmail.com>
2021-12-31 11:29:14 +0900

    add ImageBlockBuilder

--- src/block/block_image.rs
+++ src/block/block_image.rs
@@ -7,6 +7,46 @@ use serde_with::skip_serializing_none;
 pub struct ImageBlock {
     pub image_url: String,
     pub alt_text: String,
+    pub title: Option<TextBlockObject>,
     pub block_id: Option<String>,
-    pub text: Option<TextBlockObject>,
+}
+
+impl ImageBlock {
+    pub fn builder(image_url: String, alt_text: String) -> ImageBlockBuilder {
+        ImageBlockBuilder::new(image_url, alt_text)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct ImageBlockBuilder {
+    pub image_url: String,
+    pub alt_text: String,
+    pub title: Option<TextBlockObject>,
+    pub block_id: Option<String>,
+}
+
+impl ImageBlockBuilder {
+    pub fn new(image_url: String, alt_text: String) -> ImageBlockBuilder {
+        ImageBlockBuilder {
+            image_url,
+            alt_text,
+            ..Default::default()
+        }
+    }
+    pub fn title(mut self, title: TextBlockObject) -> ImageBlockBuilder {
+        self.title = Some(title);
+        self
+    }
+    pub fn block_id(mut self, block_id: String) -> ImageBlockBuilder {
+        self.block_id = Some(block_id);
+        self
+    }
+    pub fn build(self) -> ImageBlock {
+        ImageBlock {
+            image_url: self.image_url,
+            alt_text: self.alt_text,
+            title: self.title,
+            block_id: self.block_id,
+        }
+    }
 }
