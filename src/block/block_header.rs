97fcd6bf30e1667a018c1cfe434aa4f346ded70a
Gompei <shoueigonpei6028@gmail.com>
2021-12-31 11:26:03 +0900

    add HeaderBlockBuilder

--- src/block/block_header.rs
+++ src/block/block_header.rs
@@ -5,6 +5,37 @@ use serde_with::skip_serializing_none;
 #[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct HeaderBlock {
-    pub text: Option<TextBlockObject>,
+    pub text: TextBlockObject,
     pub block_id: Option<String>,
+}
+
+impl HeaderBlock {
+    pub fn builder(text: TextBlockObject) -> HeaderBlockBuilder {
+        HeaderBlockBuilder::new(text)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct HeaderBlockBuilder {
+    pub text: TextBlockObject,
+    pub block_id: Option<String>,
+}
+
+impl HeaderBlockBuilder {
+    pub fn new(text: TextBlockObject) -> HeaderBlockBuilder {
+        HeaderBlockBuilder {
+            text,
+            ..Default::default()
+        }
+    }
+    pub fn block_id(mut self, block_id: String) -> HeaderBlockBuilder {
+        self.block_id = Some(block_id);
+        self
+    }
+    pub fn build(self) -> HeaderBlock {
+        HeaderBlock {
+            text: self.text,
+            block_id: self.block_id,
+        }
+    }
 }
