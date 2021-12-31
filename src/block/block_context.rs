667a02265b4c96884d5a78f000dc199497df8598
Gompei <shoueigonpei6028@gmail.com>
2021-12-31 11:17:21 +0900

    add ContextBlockBuilder

--- src/block/block_context.rs
+++ src/block/block_context.rs
@@ -5,6 +5,37 @@ use serde_with::skip_serializing_none;
 #[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, Default, PartialEq)]
 pub struct ContextBlock {
+    pub elements: Vec<MixedElement>,
     pub block_id: Option<String>,
-    pub elements: Option<Vec<MixedElement>>,
+}
+
+impl ContextBlock {
+    pub fn builder(elements: Vec<MixedElement>) -> ContextBlockBuilder {
+        ContextBlockBuilder::new(elements)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct ContextBlockBuilder {
+    pub elements: Vec<MixedElement>,
+    pub block_id: Option<String>,
+}
+
+impl ContextBlockBuilder {
+    pub fn new(elements: Vec<MixedElement>) -> ContextBlockBuilder {
+        ContextBlockBuilder {
+            elements,
+            ..Default::default()
+        }
+    }
+    pub fn block_id(mut self, block_id: String) -> ContextBlockBuilder {
+        self.block_id = Some(block_id);
+        self
+    }
+    pub fn build(self) -> ContextBlock {
+        ContextBlock {
+            elements: self.elements,
+            block_id: self.block_id,
+        }
+    }
 }
