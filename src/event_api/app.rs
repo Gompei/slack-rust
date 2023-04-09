e40c9a35f5d0bba59c44c48b695cc30e19df1ae9
Evan Reeves <web@evanreeves.com>
2023-04-09 10:59:10 +0200

    test: adds tests for "app requested" event
    These tests are known to be failing. This demonstrates that the current
    enum implementation is incomplete compared to the examples provided by
    the Slack documentation

--- src/event_api/app.rs
+++ src/event_api/app.rs
@@ -6,6 +6,7 @@ use serde_with::skip_serializing_none;
 
 #[skip_serializing_none]
 #[derive(Deserialize, Serialize, Debug, PartialEq)]
+/// See more at <https://api.slack.com/events/app_requested>
 pub struct AppRequest {
     pub id: Option<String>,
     pub app: Option<App>,
@@ -45,4 +46,75 @@ pub struct Scope {
     pub description: Option<String>,
     pub is_sensitive: Option<bool>,
     pub token_type: Option<String>,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::event_api::event::EventType;
+    use assert_json_diff::*;
+    use serde_json::Value;
+
+    #[test]
+    fn deserialized_app_requested_event() {
+        let json = r##"
+        {
+          "type": "app_requested",
+          "app_request":{
+              "id": "1234",
+              "app": {
+                "id": "A5678",
+                "name": "Brent's app",
+                "description": "They're good apps, Bront.",
+                "help_url": "brontsapp.com",
+                "privacy_policy_url": "brontsapp.com",
+                "app_homepage_url": "brontsapp.com",
+                "app_directory_url": "https://slack.slack.com/apps/A102ARD7Y",
+                "is_app_directory_approved": true,
+                "is_internal": false,
+                "additional_info": "none"
+              },
+              "previous_resolution": {
+                 "status": "approved",
+                 "scopes": [
+                  {
+                    "name": "app_requested",
+                    "description": "allows this app to listen for app install requests",
+                    "is_sensitive": false,
+                    "token_type": "user"
+                  }]
+              },
+              "user":{
+                "id": "U1234",
+                "name": "Bront",
+                "email": "bront@brent.com"
+              },
+              "team": {
+                "id": "T1234",
+                "name": "Brant App Team",
+                "domain": "brantappteam"
+              },
+              "enterprise": null,
+              "scopes": [
+                {
+                  "name": "app_requested",
+                  "description": "allows this app to listen for app install requests",
+                  "is_sensitive": false,
+                  "token_type": "user"
+                }
+              ],
+              "message": "none"
+          }
+        }
+        "##;
+        let deserialized = serde_json::from_str::<EventType>(&json).unwrap();
+        // Make comparison between our deserialized struct and generic serde_json Value to ensure
+        // that all expected keys exist
+        let expected: Value = serde_json::from_str(&json).unwrap();
+        assert_json_eq!(deserialized, expected);
+
+        match deserialized {
+            EventType::AppRequested{..} => assert!(true),
+            _ => panic!("unrecognized variant"),
+        }
+    }
 }
